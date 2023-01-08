14904c6fa4e83b8374eb7ef717ead1438bb59506
Fabio <fabiogodoy955@icloud.com>
2023-01-08 17:10:16 -0300

    chore: add error class

---
+++ src/errors/errors.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+class AppError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number = 400) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export const errorHandler = (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  return response.status(500).json({ message: "Internal Server Error." });
+};
+
+export { AppError };
