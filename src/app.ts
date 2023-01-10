9801b23ad73be4b909f60f28b0fe6370913e98e0
wilkeran <wilkeran.costs@gmail.com>
2023-01-10 16:25:41 -0300

    feat: created feature create/list product

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import productRouter from "./router/product.routes";
 import { router } from "./router/users.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+
+app.use('/products', productRouter)
 
 export default app;
--- src/controlles/products/products.controllers.ts
+++ src/controlles/products/products.controllers.ts
@@ -1,7 +1,23 @@
+import createProductsServices, { IproductRequest } from '../../services/createProducts.services';
 import { Request, Response } from "express";
+import listProductsServices from '../../services/listProducts.services';
 
-export const createProductController = (req: Request, res: Response) => {
-  return res.status(200).json({});
+
+
+export const createProductController = async (req: Request, res: Response) => {
+  
+  const productData: IproductRequest = req.body
+  const newProduct = await createProductsServices(productData)
+  
+  return res.status(200).json({newProduct})
+};
+
+export const listProductController = async (req:Request, res: Response) => {
+
+  const products = await listProductsServices()
+
+  return res.json(products)
+
 };
 
 export const updateProductController = (req: Request, res: Response) => {
