771f0e1e083902c5f64d6bcef8a65973ce6776fb
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-01-10 16:24:10 -0300

    feat: create function to update and delete a product

--- src/controlles/products/products.controllers.ts
+++ src/controlles/products/products.controllers.ts
@@ -1,13 +1,35 @@
 import { Request, Response } from "express";
+import { deleteProductService } from "../../services/deleteProduct.service";
+import { updateProductService } from "../../services/updateProduct.service";
+
+interface iProductUpdateRequest{
+  "name"?: string,
+  "description"?: string,
+  "price"?: number,
+  "amount"?: number,
+  "avaible"?: boolean
+}
 
 export const createProductController = (req: Request, res: Response) => {
   return res.status(200).json({});
 };
 
-export const updateProductController = (req: Request, res: Response) => {
-  return res.status(200).json({});
+export const updateProductController = async (req: Request, res: Response) => {
+
+  const idProduct = Number(req.params.id)
+  const dataBody: iProductUpdateRequest = req.body
+
+  const updatedBody = await updateProductService(idProduct, dataBody)
+
+  return res.status(204).json(updatedBody);
 };
 
-export const deleteProductController = (req: Request, res: Response) => {
-  return res.status(200).json({});
+export const deleteProductController = async (req: Request, res: Response) => {
+
+  const idProduct = Number(req.params.id)
+
+  await deleteProductService(idProduct)
+
+  return res.status(200);
+  
 };
