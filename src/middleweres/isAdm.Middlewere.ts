90f853e42d6403966916d6259d6bfd3e2b1baf7e
Fabio <fabiogodoy955@icloud.com>
2023-01-11 15:03:35 -0300

    fix: isAdmMiddlewere

--- src/middleweres/isAdm.Middlewere.ts
+++ src/middleweres/isAdm.Middlewere.ts
@@ -1,34 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import AppDataSource from "../data-source";
-import { User } from "../entities/user.entity";
 
 export const isAdmMiddlewere = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const authToken = req.headers.authorization;
+  const isAdm = req.user.isAdm;
 
-  const token = authToken!.split(" ")[1];
-
-  return jwt.verify(
-    token as string,
-    process.env.SECRETKEY,
-    async (error, decoded: any) => {
-      if (error) {
-        return res.send(error.message);
-      }
-
-      const userRepository = AppDataSource.getRepository(User);
-
-      const user = await userRepository.findOneBy({ id: decoded.sub });
-
-      if (!user?.isAdm) {
-        return res.status(403).json({ message: "Need admin permission" });
-      }
-
-      next();
-    }
-  );
+  if (!isAdm) {
+    return res.status(403).json({ message: "You are not a admin" });
+  }
+  next();
 };
