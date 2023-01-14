77881cd778f03037cd097ae088cd7877278f6d85
Fabio <fabiogodoy955@icloud.com>
2023-01-14 15:52:54 -0300

    feat: send email user creation

--- src/services/orders/createOrder.service.ts
+++ src/services/orders/createOrder.service.ts
@@ -1,12 +1,12 @@
+import { create } from "domain";
 import AppDataSource from "../../data-source";
+import { IEmailRequest } from "../../email.interface";
 import { Orders } from "../../entities/orders.entity";
 import { OrdersProducts } from "../../entities/ordersProducts.entity";
 import { User } from "../../entities/user.entity";
+import { sendEmail } from "../../nodemailer.util";
 
-const createOrderService = async (
-  dataOrder: any,
-  idUser: string
-): Promise<{}> => {
+const createOrderService = async (dataOrder, idUser) => {
   const userRepository = AppDataSource.getRepository(User);
   const orderRepository = AppDataSource.getRepository(Orders);
   const orderProductsRepository = AppDataSource.getRepository(OrdersProducts);
@@ -26,6 +26,19 @@ const createOrderService = async (
     });
     await orderProductsRepository.save(newOrdersProduct);
   });
+
+  try {
+    const email: IEmailRequest = {
+      subject: `Bem vindo ${user.name}!`,
+      text: "Compra realizada com sucesso!",
+      to: user.email,
+      html: '<div style="background-color: red"><h1>Hello</h1></div>',
+    };
+
+    await sendEmail(email);
+  } catch (error) {
+    console.log(error);
+  }
 
   return { message: "order created successfully" };
 };
