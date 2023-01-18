dd82fd704fe1c7b9a2b68d7d96309c68dc2ac14e
Gabriel Garcia <102615441+lonq1@users.noreply.github.com>
2023-01-18 10:16:45 -0300

    Fix/verify products cart middleware (#31)
    * feat: POST /orders amount, stock and available validations
    
    * fix: verifyProductsCart async await in loops
    
    Co-authored-by: Fabio Godoy <106690816+FabioRGodoy@users.noreply.github.com>

--- src/middleweres/verifyProductsCart.middleware.ts
+++ src/middleweres/verifyProductsCart.middleware.ts
@@ -1,19 +1,26 @@
-import {Request,Response,NextFunction} from "express"
-import AppDataSource from "../data-source"
-import { Products } from "../entities/products.entity"
+import { Request, Response, NextFunction } from "express";
+import AppDataSource from "../data-source";
+import { Products } from "../entities/products.entity";
+import { AppError } from "../errors/errors";
 
-const verifyProductCartMiddleware =async(req:Request,res:Response,next:NextFunction)=>{
-    const productsRepository = AppDataSource.getRepository(Products)
-      
-    req.body.map( async(product)=>{
-      const findProduct = await productsRepository.findOneBy({id: product.product})
-        if(!findProduct){
-           return res.status(404).json({message:"Product not found"})
-        }
-    })
+const verifyProductCartMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const productsRepository = AppDataSource.getRepository(Products);
 
-    next()
+  for (let i = 0; i < req.body.length; i++) {
+    const order = await productsRepository.findOneBy({
+      id: req.body[i].product,
+    });
 
-}
+    if (!order) {
+      return res.status(404).json({ message: "Product doesn't exists" });
+    }
+  }
 
-export default verifyProductCartMiddleware
+  next();
+};
+
+export default verifyProductCartMiddleware;
